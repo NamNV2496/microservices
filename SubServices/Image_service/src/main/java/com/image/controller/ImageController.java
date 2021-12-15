8783cc516ee646a481129c5b26dc3e5ca1dab865
namnv2496 <namnv2496@gmail.com>
2021-12-15 22:40:00 +0700

    add zuul service

--- SubServices/Image_service/src/main/java/com/image/controller/ImageController.java
+++ SubServices/Image_service/src/main/java/com/image/controller/ImageController.java
@@ -1,17 +1,18 @@
 package com.image.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ImageController {
+    @RequestMapping("/")
+    public void imageRoot() {
+        System.out.println("this is test");
+    }
     @GetMapping("/testGet")
-    public String imageGet() {
-        return "Test get mapping API of image service";
+    public void imageGet() {
+        System.out.println("Test get mapping API of image service");
     }
 
-    @GetMapping("/testHome")
-    public String imageHome() {
-        return "Test get mapping API of image service";
-    }
 }
---
+++ Zuul_Server/src/main/java/com/zuul_server/ZuulServerApplication.java
@@ -0,0 +1,20 @@
+package com.zuul_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.web.ZuulHandlerMapping;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServerApplication.class, args);
+    }
+
+}
